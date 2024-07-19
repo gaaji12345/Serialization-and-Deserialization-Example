cb14fe7d0f5f0025223e90c2ca568c9cb9cd11e0
gaji12345 <gajimax12345@gmail.com>
2024-07-19 10:07:43 +0530

    first commit

---
+++ src/main/java/DeserilizeCustomer.java
@@ -0,0 +1,15 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DeserilizeCustomer {
+    public static void main(String[] args) {
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            Employee employee = mapper.readValue(new File("employee.json"), Employee.class);
+//            System.out.println("Employee deserialized from 'employee.json'");
+//            System.out.println(employee);
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+//    }
+    }
+}
---
+++ src/main/java/SerilizeCustomer.java
@@ -0,0 +1,2 @@
+public class SerilizeCustomer {
+}
